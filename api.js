55dd9fa1ea913cc04811d23f7a00d76f2729892a
dlourennco <davidlimalourenco@gmail.com>
2020-03-25 21:32:13 -0300

    covid-19 no brasil

---
+++ api.js
@@ -0,0 +1,30 @@
+let apiUrl = 'https://thevirustracker.com/free-api?countryTotal=BR'
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    let country = data.countrydata[0]
+    let { 
+      total_cases, 
+      total_recovered, 
+      total_deaths, 
+      total_new_cases_today, 
+      total_new_deaths_today, 
+      total_active_cases 
+    } = country
+
+    document.getElementById('casos-totais-span').innerText = total_cases
+    document.getElementById('novos-casos-hoje-span').innerText = total_new_cases_today
+    document.getElementById('mortes-span').innerText = total_deaths
+    document.getElementById('novas-mortes-hoje-span').innerText = total_new_deaths_today
+    document.getElementById('casos-ativos-span').innerText = total_active_cases
+    document.getElementById('casos-curados-span').innerText = total_recovered
+
+    console.log(total_cases)
+    console.log(total_recovered)
+    console.log(total_deaths)
+    console.log(total_new_cases_today)
+    console.log(total_new_deaths_today)
+    console.log(total_active_cases)
+  })
+
